adbd32f746d74ac297f94830bb70002cf4746ef4
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-10 21:17:01 +1300

    gh=pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://tulza.github.io/vite-react-ts-template/",
   plugins: [react()],
-})
+});
