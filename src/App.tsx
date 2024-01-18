7db64755d9dc2a9328769a6377731cdec30d0cf5
Tulza <40633228+tulza@users.noreply.github.com>
2024-01-19 03:07:31 +1300

    vite-tsconfig and routerv6 installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import Home from "./Template/Home";
-import MathGame from "./Template/MathGame";
-import Navigation from "./Template/Navigation";
-import Template from "./Template/Template";
-import "./styles.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "@components/Home";
+import MathGame from "@components/MathGame";
+import Navigation from "@components/Navigation";
+import Template from "@components/Template";
+import "@styles/styles.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <Router basename="/vite-react-ts-template">
       <div className="App flex h-[100vh] flex-col justify-between">
         <div className="Content m-20 flex h-[800px]  flex-col justify-center bg-black outline">
-          <Switch>
-            <Route exact path="/">
-              <Template />
-            </Route>
-            <Route path="/home">
-              <Home />
-            </Route>
-            <Route path="/mathgame">
-              <MathGame />
-            </Route>
-          </Switch>
+          <Routes>
+            <Route path="/" element={<Template />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/mathgame" element={<MathGame />} />
+          </Routes>
         </div>
         <Navigation />
       </div>
